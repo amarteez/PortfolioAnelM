822f501f793e289a3cdf3dea337b08342295a5d2
amarteez <anelmartez18@gmail.com>
2026-05-14 22:40:58 -0500

    change info

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ const About = ({ language }) => {
       <h2>{language === 'es' ? '☁️☁️☁️' : '🌧️🌧️🌧️'}</h2>
       <p>
         {language === 'es'
-          ? 'La experiencia en tecnología no se mide solo en años, sino en la capacidad de enfrentar desafíos con ingenio y persistencia. Aunque mi trayectoria es aún joven, mi verdadera motivación proviene de una intensa pasión por resolver problemas complejos y transformar ideas en soluciones prácticas. Cada proyecto representa una oportunidad para avanzar hacia la mejora continua y la innovación.'
-          : 'Experience in technology is not measured merely by years but by the ability to tackle challenges with ingenuity and persistence. While my journey is still young, my true motivation comes from a deep passion for solving complex problems and turning ideas into practical solutions. Each project represents an opportunity to move towards continuous improvement and innovation.'}
-      </p>
+          ? 'La experiencia en tecnología no se mide solo en años, sino en la capacidad de resolver problemas con ingenio y persistencia. Con más de 2.5 años de trayectoria en sectores de alta exigencia como finanzas gubernamentales e ingeniería, me especializo en transformar desafíos complejos en soluciones escalables y arquitectura backend sólida.'
+          : 'Experience in technology is not measured merely by years but by the ability to solve problems with ingenuity and persistence. With over 2.5 years of experience in high-demand sectors such as government finance and engineering, I specialize in transforming complex challenges into scalable solutions and solid backend architecture.'
+          }      </p>
     </div>
   );
 };
